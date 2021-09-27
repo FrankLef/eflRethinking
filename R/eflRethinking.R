9578d65309b111e4d7e90000ae90bec831298440
FrankLef <flefebvre01@hotmail.com>
2021-09-27 11:52:56 -0400

    initial

---
+++ R/eflRethinking.R
@@ -0,0 +1,8 @@
+#' eflRethinking: A package with functions to use with Rethinking, 2nd edition
+#'
+#' The eflRethinking package provides functions mainly to solve practices
+#' of Rethinking, 2nd edition
+#'
+#' @docType package
+#' @name eflRethinking
+NULL
--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,23 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+
+#' Example function that shows message that begins with "Hello" to console.
+#'
+#' @param msg String with a message that begins with "Hello". Default message
+#'  is "Hello, world!".
+#'
+#' @return A message to console that starts with "Hello"
+hello <- function(msg = "Hello, world!") {
+  if (grepl(pattern = "^hello.*", x = msg, ignore.case = TRUE)) {
+    message(msg)
+  } else {
+    msg_head <- cli::col_yellow("Message must start with \"hello\"")
+    msg_body <- c("i" = sprintf("Message: %s", msg))
+    msg <- paste(msg_head, rlang::format_error_bullets(msg_body), sep = "\n")
+    rlang::abort(
+      message = msg,
+      class = "hello_error")
+  }
+  invisible(msg)
 }
